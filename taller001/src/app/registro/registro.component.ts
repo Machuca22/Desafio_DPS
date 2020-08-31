a6a5794f8219351c1270e9057e9ca18e6353c53c
Obispo95 <irvingobispo1@gmail.com>
2020-08-30 22:16:43 -0600

    Firebase Push, counter added.
    Firebase installation, counter added for visitors.

--- taller001/src/app/registro/registro.component.ts
+++ taller001/src/app/registro/registro.component.ts
@@ -1,36 +1,80 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
- @Component({
+
+@Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
    styleUrls: ['./registro.component.css']
 })
+
 export class RegistroComponent implements OnInit {
+  
   registro = [];
   cliente: any;
   nombre: string;
   dui :string;
   Vehiculo : string;
   costo:number;
-  mayor: string; 
-  contador: number; constructor() {
+  mayor: string;  constructor() {
   }
+
   ngOnInit() {
     this.nombre = '';
     this.Vehiculo = '';
     this.dui = '';
     this.costo = 0;
-    this.contador = 0;
-  } ingresar() {
-     this.cliente = {
-      "nombre": this.nombre,
-      "dui": this.dui,
-      "Vehiculo": this.Vehiculo,
-      "costo": this.costo,
-      "mayor": this.mayor
-    };
+  } 
+  
+  ingresar() {
+    let contador = 0;
+    let descuento:number=0;
+    let nuevoCosto:number=0;
+
+    for(let i=0; i<this.registro.length; i++){
+        if(this.dui == this.registro[i].dui){
+          contador++;
+        }
+    }
+
+    if(contador <=0){
+      this.cliente = {
+        "nombre":this.nombre,
+        "dui":this.dui,
+        "Vehiculo":this.Vehiculo,
+        "costo":this.costo,
+        "Descuento":descuento,
+        "Total":this.costo-descuento
+      }
+      this.registro.push(this.cliente);
+      contador++;
+    } else if (contador >=1 && contador <4) {
+      descuento = 5;
+      nuevoCosto = this.costo - (this.costo*0.05);
+      this.cliente = {
+        "nombre":this.nombre,
+        "dui":this.dui,
+        "Vehiculo":this.Vehiculo,
+        "costo":nuevoCosto,
+        "Descuento":descuento,
+        "Total":nuevoCosto
+    }
     this.registro.push(this.cliente);
-    this.contador++;
+    contador++;
+  }else if (contador>=4){
+    descuento = 10;
+      nuevoCosto = this.costo - (this.costo*0.10);
+      this.cliente = {
+        "nombre":this.nombre,
+        "dui":this.dui,
+        "Vehiculo":this.Vehiculo,
+        "costo":nuevoCosto,
+        "Descuento":descuento,
+        "Total":nuevoCosto
   }
+  this.registro.push(this.cliente);
+  contador++;
+  }
+}
+
 }
